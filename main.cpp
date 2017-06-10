f996be0b00b07d7c3893709ceff28f5b9fce568d
kirill146 <kirill1234565@gmail.com>
2017-06-10 14:53:07 +0300

    bugfix

--- main.cpp
+++ main.cpp
@@ -14,21 +14,24 @@ long long encode(const char* input, const char* output) {
     if (!inputFile) {
         throw std::invalid_argument("can not open input file");
     }
-    unsigned char* buf = new unsigned char[size_of_block];
+    FILE* outputFile = fopen(output, "wb");
+    if (!outputFile) {
+        throw std::invalid_argument("can not open output file");
+    }
+    unsigned char* buf = static_cast<unsigned char*> (operator new(size_of_block));
     vector<long long> cnt(256);
     size_t size;
     size_t cnt_of_blocks = 0;
     while ((size = fread(buf, 1, size_of_block, inputFile)) == size_of_block) {
-        for (int j = 0; j < size_of_block; ++j) {
+        for (size_t j = 0; j < size_of_block; ++j) {
             ++cnt[buf[j]];
         }
         ++cnt_of_blocks;
     }
-    for (int j = 0; j < size; ++j) {
+    for (size_t j = 0; j < size; ++j) {
         ++cnt[buf[j]];
     }
-    long long len_of_file = (long long)cnt_of_blocks * size_of_block + size;
-    long long len_of_last_block = size;
+    long long len_of_file = (long long) cnt_of_blocks * size_of_block + size;
     if (size) {
         ++cnt_of_blocks;
     }
@@ -36,20 +39,19 @@ long long encode(const char* input, const char* output) {
 
     huffman huff;
     huff.build_tree(cnt);
-    FILE* outputFile = fopen(output, "wb");
-    if (!outputFile) {
-        throw std::invalid_argument("can not open output file");
-    }
-    for (int i = 0; i < cnt_of_blocks; ++i) {
-        size = (size_t) (i == cnt_of_blocks - 1 ? len_of_last_block : size_of_block);
-        fread(buf, 1, size, inputFile);
-        huff.encode(buf, size, (i == cnt_of_blocks - 1));
-        char* buff = (char*)huff.get_buffer();
-        fwrite(buff, 1, huff.get_size_of_buffer(), outputFile);
+    for (size_t i = 0; i < cnt_of_blocks; ++i) {
+        size = fread(buf, 1, size_of_block, inputFile);
+        try {
+            huff.encode(buf, size, (i == cnt_of_blocks - 1));
+        } catch (std::invalid_argument const& e) {
+            operator delete(buf);
+            throw e;
+        }
+        fwrite((char*)huff.get_buffer(), 1, huff.get_size_of_buffer(), outputFile);
     }
     fclose(inputFile);
     fclose(outputFile);
-    delete[] buf;
+    operator delete(buf);
     return len_of_file;
 }
 
@@ -58,33 +60,36 @@ long long decode(const char* input, const char* output) {
     if (!inputFile) {
         throw std::invalid_argument("can not open input file");
     }
-    unsigned char* buf = new unsigned char[size_of_block];
+    FILE* outputFile = fopen(output, "wb");
+    if (!outputFile) {
+        throw std::invalid_argument("can not open output file");
+    }
+    unsigned char* buf = static_cast<unsigned char*> (operator new(size_of_block));
     size_t size;
     size_t cnt_of_blocks = 0;
     while ((size = fread(buf, 1, size_of_block, inputFile)) == size_of_block) {
         ++cnt_of_blocks;
     }
     long long len_of_file = (long long)cnt_of_blocks * size_of_block + size;
-    long long len_of_last_block = size;
     if (size) {
         ++cnt_of_blocks;
     }
     rewind(inputFile);
 
     huffman huff;
-    FILE* outputFile = fopen(output, "wb");
-    if (!outputFile) {
-        throw std::invalid_argument("can not open output file");
-    }
-    for (int i = 0; i < cnt_of_blocks; ++i) {
-        size = (size_t) (i == cnt_of_blocks - 1 ? len_of_last_block : size_of_block);
-        fread(buf, 1, size, inputFile);
-        huff.decode(buf, size, (i == cnt_of_blocks - 1));
+    for (size_t i = 0; i < cnt_of_blocks; ++i) {
+        size = fread(buf, 1, size_of_block, inputFile);
+        try {
+            huff.decode(buf, size, (i == cnt_of_blocks - 1));
+        } catch (std::invalid_argument const& e) {
+            operator delete(buf);
+            throw e;
+        }
         fwrite((char*)huff.get_buffer(), 1, huff.get_size_of_buffer(), outputFile);
     }
     fclose(inputFile);
     fclose(outputFile);
-    delete[] buf;
+    operator delete(buf);
     return len_of_file;
 }
 
